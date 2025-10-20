208dbde1eedcdee9d489767d34436403a7826da3
DiegoPerezPonce <diegoperezponce96@gmail.com>
2025-10-20 16:48:31 +0200

    DB created!

---
+++ src/Entity/Nurse.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NurseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NurseRepository::class)]
+class Nurse
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $user = null;
+
+    #[ORM\Column(length: 70)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $pw = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?string
+    {
+        return $this->user;
+    }
+
+    public function setUser(string $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPw(): ?string
+    {
+        return $this->pw;
+    }
+
+    public function setPw(string $pw): static
+    {
+        $this->pw = $pw;
+
+        return $this;
+    }
+}
